dede8f011fdeebeb4e8a53e78d8c877fec433e85
Raghu K <raghuk.vit@gmail.com>
2024-10-22 08:53:34 +0530

    Terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "sg" {
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "${var.component_name}-${var.env}"
   }
