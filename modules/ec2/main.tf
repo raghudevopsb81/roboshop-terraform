c770a32abecd678cf84e29143ed4b307dc71004c
SVR Kalluru <raghuk.vit@gmail.com>
2024-11-01 08:51:19 +0530

    Project RoboShop

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -44,7 +44,6 @@ resource "null_resource" "ansible-pull" {
     }
 
     inline = [
-      "sudo labauto ansible",
       "ansible-pull -i localhost, -U https://github.com/raghudevopsb81/roboshop-ansible roboshop.yml -e env=${var.env} -e component=${var.component_name} -e vault_token=${var.vault_token}"
     ]
   }
