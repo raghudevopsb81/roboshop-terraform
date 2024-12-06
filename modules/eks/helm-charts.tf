2f32d2db15c66c64de2c2a32b0d5dd449475ca57
SVR Kalluru <raghuk.vit@gmail.com>
2024-12-06 08:17:00 +0530

    Project RoboShop

--- modules/eks/helm-charts.tf
+++ modules/eks/helm-charts.tf
@@ -1,5 +1,21 @@
+resource "null_resource" "kube-bootstrap" {
+  provisioner "local-exec" {
+    command =<<EOF
+aws eks update-kubeconfig  --name ${var.env}-eks
+kubectl create ns devops
+EOF
+  }
+}
+
 resource "helm_release" "nginx-ingress" {
+  depends_on = [null_resource.kube-bootstrap]
   chart = "oci://ghcr.io/nginxinc/charts/nginx-ingress"
   name  = "nginx-ingress"
+  namespace = "devops"
+  wait       = true
+
+  values = [
+    file("${path.module}/helm-config/nginx-ingress.yml")
+  ]
 }
 
