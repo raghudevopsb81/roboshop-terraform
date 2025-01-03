3fa41d30281ee48e1afe1225d87aa57849eefc42
Raghu K <raghuk.vit@gmail.com>
2025-01-03 08:34:52 +0530

    RoboShop

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
-provider "vault" {
-  address         = "http://vault-internal.rdevopsb81.online:8200"
-  token           = var.vault_token
-  skip_tls_verify = true
-}
+# provider "vault" {
+#   address         = "http://vault-internal.rdevopsb81.online:8200"
+#   token           = var.vault_token
+#   skip_tls_verify = true
+# }
 
 provider "helm" {
   kubernetes {
