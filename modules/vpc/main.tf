4f71ece3a1b0eeec4fdb2dd40fbe05251d30610a
Raghu K <raghuk.vit@gmail.com>
2025-01-03 08:49:55 +0530

    RoboShop

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,3 +19,13 @@ resource "aws_vpc_peering_connection" "main" {
   vpc_id      = aws_vpc.main.id
   auto_accept = true
 }
+
+resource "aws_route" "main" {
+  route_table_id         = aws_vpc.main.default_route_table_id
+  destination_cidr_block = var.default_vpc["cidr"]
+}
+
+resource "aws_route" "default-vpc-route-table" {
+  route_table_id         = var.default_vpc["route_table"]
+  destination_cidr_block = var.cidr_block
+}
