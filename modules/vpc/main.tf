e9bccad889fed75e28757963566cb8d34152f7b4
SVR Kalluru <raghuk.vit@gmail.com>
2025-01-06 08:05:37 +0530

    Project RoboShop

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,8 +45,7 @@ resource "aws_route" "main" {
 }
 
 resource "aws_route" "igw" {
-  for_each                  = var.subnets
-  route_table_id            = aws_route_table.main[each.key].id
+  route_table_id            = aws_route_table.main["public"].id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.main.id
 }
