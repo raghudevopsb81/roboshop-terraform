3150328fa18c88704539998d83451ac5d2a98211
SVR Kalluru <raghuk.vit@gmail.com>
2025-01-06 07:49:45 +0530

    Project RoboShop

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,6 +14,14 @@ resource "aws_subnet" "subnet" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
 resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id = var.default_vpc["id"]
   vpc_id      = aws_vpc.main.id
@@ -24,6 +32,12 @@ resource "aws_route" "main" {
   route_table_id            = aws_vpc.main.default_route_table_id
   destination_cidr_block    = var.default_vpc["cidr"]
   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+}
+
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.main.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.main.id
 }
 
 resource "aws_route" "default-vpc-route-table" {
