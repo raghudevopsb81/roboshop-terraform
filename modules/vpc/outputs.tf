48be44bd0089d5dd3d2a702df6ac32bbffd944b6
SVR Kalluru <raghuk.vit@gmail.com>
2025-01-07 08:01:12 +0530

    Project RoboShop

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,9 +2,9 @@ output "igw" {
   value = local.igw_enabled_subnets
 }
 
-# output "db_subnets" {
-#   value = [ for subnet_key, subnet_value in aws_subnet.main : subnet_value.id if subnet_value.subnet_group == "db" ]
-# }
+output "db_subnets" {
+  value = [ for subnet_key, subnet_value in aws_subnet.main : subnet_value.id if subnet_value.tags["subnet_group"] == "db" ]
+}
 
 output "subnets" {
   value = aws_subnet.main
